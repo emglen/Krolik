d54f075554c3da7aed7c7444e0228f23c559d6e4
solep24209 <solep24209@cnetmail.net>
2020-02-11 14:43:45 +0200

    Fix pom

--- src/main/java/AbstractParentTest/AbstractParentTest.java
+++ src/main/java/AbstractParentTest/AbstractParentTest.java
@@ -1,9 +1,9 @@
 package AbstractParentTest;
 
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
